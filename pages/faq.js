231b34dc29325f6006601f677623bcfa364df767
YSMDeveloper <ysmdeveloper@gmail.com>
2022-08-01 17:56:59 -0400

    01-08-2022

--- pages/faq.js
+++ pages/faq.js
@@ -1,5 +1,14 @@
+import Link from 'next/link'
+
 export default function FAQ(){
-    return <div>
+    return (
+    <div>
         <h1>FAQ</h1>
+        <p>
+            <Link href="/">
+                <a>Ir a Home</a>
+            </Link>
+        </p>
     </div>
+    );  
 }
